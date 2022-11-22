cc76a9ca32a8d5b0154328748aee79cafe64cc56
Danil336 <barsecdebil@gmail.com>
2022-11-22 21:13:03 +0100

    site page

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-// TS ФАЙЛ ТОЛЬКО ДЛЯ ИМПОРТОВ ВСЕХ JS ФАЙЛОВ, У МЕНЯ ТАКОЙ КОНФИГ
-
-import "./styles/header.scss";
-import "./styles/main.scss";
-import "./styles/startPage.scss";
-import "./styles/galleryPage.scss";
-import "./scripts/app.js";
-import "./scripts/gallery.js"
